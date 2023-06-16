d83af5f421bee8b8e5f7345f42e4c85222d986b4
Robertino Martinez <48034748+rober-m@users.noreply.github.com>
2023-06-16 17:04:16 +0000

    updated utilities and MintCertificate to work with new workflow

--- code/Certification/frontend/src/components/MintCertificate.tsx
+++ code/Certification/frontend/src/components/MintCertificate.tsx
@@ -1,77 +1,33 @@
 import {
     Address,
-    AddressDetails,
     Constr,
     PolicyId,
+    SpendingValidator,
     Unit,
     getAddressDetails,
-    toLabel,
     toUnit,
 } from "lucid-cardano";
 import React, { useContext, useEffect, useState } from "react";
-import { applyParamsToScript, Data, MintingPolicy } from "lucid-cardano";
+import { Data, MintingPolicy } from "lucid-cardano";
 import { AppStateContext } from "@/pages/_app";
 import {
     signAndSubmitTx,
     genPersolanizedMetadata,
-    genMerkleRoot,
-    genMerkleProof,
+    genMintMerkleProof,
 } from "@/utilities/utilities";
-import {
-    CERTIFICATE_NFT_SCRIPT,
-    GENERIC_METADATA,
-    THREAD_TOKEN_MP,
-    TOKEN_LOCK_VAL,
-} from "@/utilities/constants";
-import { Addr, CertifParameters, MerkleProof } from "@/utilities/types";
+import { MerkleProof } from "@/utilities/types";
+import setupData from "@/data/setupData";
+import genericMetadata from "@/data/generic-NFT-metadata.json" assert { type: "json" };
 
 export default function MintCertificate() {
     const { appState, setAppState } = useContext(AppStateContext);
     const { lucid, wAddr } = appState;
     const [name, setName] = useState<string>("");
-    const [metadata, setMetadata] = useState(GENERIC_METADATA);
+    const [metadata, setMetadata] = useState(genericMetadata);
 
     useEffect(() => {
-        setMetadata(genPersolanizedMetadata(name));
+        name && setMetadata(genPersolanizedMetadata(name));
     }, [name]);
-
-    const getFinalCertificateMP = (): {
-        lockingValAddress: Address;
-        certNftMP: MintingPolicy;
-    } => {
-        const lockingValAddress: Address =
-            lucid!.utils.validatorToAddress(TOKEN_LOCK_VAL);
-        const lockingAddressDetails: AddressDetails =
-            getAddressDetails(lockingValAddress);
-        const addrAlwaysFail: Addr = {
-            addressCredential: {
-                ScriptCredential: [
-                    lockingAddressDetails.paymentCredential!.hash,
-                ],
-            },
-            addressStakingCredential: null,
-        };
-
-        const parameters: CertifParameters = {
-            merkleRoot: genMerkleRoot(),
-            prefixNFT: toLabel(222),
-            prefixRef: toLabel(100),
-            threadSymbol: lucid!.utils.mintingPolicyToId(THREAD_TOKEN_MP),
-            lockAddress: addrAlwaysFail,
-        };
-
-        const Params = Data.Tuple([CertifParameters]);
-        type Params = Data.Static<typeof Params>;
-        const certNftMP: MintingPolicy = {
-            type: "PlutusV2",
-            script: applyParamsToScript<Params>(
-                CERTIFICATE_NFT_SCRIPT,
-                [parameters],
-                Params
-            ),
-        };
-        return { lockingValAddress, certNftMP };
-    };
 
     const mintCertNFT = async () => {
         console.log("minting NFT for " + wAddr);
@@ -80,15 +36,24 @@ export default function MintCertificate() {
                 getAddressDetails(wAddr).paymentCredential?.hash || "";
 
             // Thread token
+            const threadScript: MintingPolicy =
+                setupData.threadScript as MintingPolicy;
             const threadTknPolicyID: PolicyId =
-                lucid.utils.mintingPolicyToId(THREAD_TOKEN_MP);
+                lucid.utils.mintingPolicyToId(threadScript);
             const threadTkn: Unit = toUnit(threadTknPolicyID, pkh);
 
+            // Locking validator (this locks the reference token)
+            const lockingValidator: SpendingValidator =
+                setupData.lockingValidator as SpendingValidator;
+            const lockingValAddress: Address =
+                lucid.utils.validatorToAddress(lockingValidator);
+
             // Certificate
-            const { lockingValAddress, certNftMP } =
-                await getFinalCertificateMP();
+            const certNftMP: MintingPolicy =
+                setupData.mintingScriptNFT as MintingPolicy;
             const certNftPolicyID: PolicyId =
                 lucid.utils.mintingPolicyToId(certNftMP);
+            console.log("PolicyID: " + certNftPolicyID);
             const userTkn: Unit = toUnit(certNftPolicyID, pkh, 222);
             const refTkn: Unit = toUnit(certNftPolicyID, pkh, 100);
 
@@ -96,7 +61,7 @@ export default function MintCertificate() {
             const Redeemer = Data.Object({ proof: MerkleProof });
             type Redeemer = Data.Static<typeof Redeemer>;
             const redeemer: Redeemer = {
-                proof: await genMerkleProof(name, pkh),
+                proof: await genMintMerkleProof(name, pkh),
             };
 
             // Transaction
@@ -107,8 +72,8 @@ export default function MintCertificate() {
                     Data.to<Redeemer>(redeemer, Redeemer)
                 )
                 .attachMintingPolicy(certNftMP)
-                .mintAssets({ [threadTkn]: -1n }, Data.void())
-                .attachMintingPolicy(THREAD_TOKEN_MP)
+                .mintAssets({ [threadTkn]: -1n }, Data.to(new Constr(1, [])))
+                .attachMintingPolicy(threadScript)
                 .payToContract(
                     lockingValAddress,
                     Data.to(new Constr(0, [Data.fromJson(metadata)])),
@@ -121,29 +86,8 @@ export default function MintCertificate() {
         }
     };
 
-    async function mintThreadToken() {
-        if (wAddr && lucid) {
-            const pkh: string =
-                getAddressDetails(wAddr).paymentCredential?.hash || "";
-
-            // Thread token
-            const threadTknPolicyID: PolicyId =
-                lucid.utils.mintingPolicyToId(THREAD_TOKEN_MP);
-            const threadTkn: Unit = toUnit(threadTknPolicyID, pkh);
-
-            // Transaction
-            const tx = await lucid
-                .newTx()
-                .mintAssets({ [threadTkn]: 1n }, Data.void())
-                .attachMintingPolicy(THREAD_TOKEN_MP)
-                .complete();
-
-            await signAndSubmitTx(tx);
-        }
-    }
-
     return (
-        <div>
+        <div className="flex flex-col">
             <div className="flex flex-row w-full justify-center items-center my-8 text-lg text-zinc-800 font-quicksand ">
                 <p>Participant&apos;s name as shared with IOG:</p>
                 <input
@@ -155,17 +99,8 @@ export default function MintCertificate() {
             </div>
 
             <button
-                onClick={mintThreadToken}
-                disabled={!wAddr}
-                className=" bg-zinc-800 text-white font-quicksand text-lg font-bold py-3 px-8 mr-5 rounded-lg shadow-[0_5px_0px_0px_rgba(0,0,0,0.6)] active:translate-y-[2px] active:shadow-[0_4px_0px_0px_rgba(0,0,0,0.6)] disabled:active:translate-y-0 disabled:active:shadow-[0_5px_0px_0px_rgba(0,0,0,0.2)] disabled:bg-zinc-200 disabled:shadow-[0_5px_0px_0px_rgba(0,0,0,0.2)] disabled:text-zinc-600"
-            >
-                {" "}
-                Mint Thread Toekn
-            </button>
-
-            <button
                 onClick={mintCertNFT}
-                disabled={!wAddr}
+                disabled={!wAddr || !lucid || !name}
                 className=" bg-zinc-800 text-white font-quicksand text-lg font-bold py-3 px-8 rounded-lg shadow-[0_5px_0px_0px_rgba(0,0,0,0.6)] active:translate-y-[2px] active:shadow-[0_4px_0px_0px_rgba(0,0,0,0.6)] disabled:active:translate-y-0 disabled:active:shadow-[0_5px_0px_0px_rgba(0,0,0,0.2)] disabled:bg-zinc-200 disabled:shadow-[0_5px_0px_0px_rgba(0,0,0,0.2)] disabled:text-zinc-600"
             >
                 {" "}
--- code/Certification/frontend/src/utilities/utilities.ts
+++ code/Certification/frontend/src/utilities/utilities.ts
@@ -1,15 +1,23 @@
 import {
+    Address,
+    C,
     Constr,
     Data,
     Lucid,
     MerkleTree,
+    SpendingValidator,
     TxComplete,
     TxHash,
+    UTxO,
+    Unit,
     fromHex,
     toHex,
 } from "lucid-cardano";
-import { GENERIC_METADATA, MERKLE_DATA } from "./constants";
 import { Hash32, MerkleProof } from "./types";
+import setupData from "@/data/setupData";
+import genericMetadata from "@/data/generic-NFT-metadata.json" assert { type: "json" };
+import mintMerkleTreeData from "@/data/mint-merkleTree-Data";
+import startMerkleTreeData from "../data/start-merkleTree-Data";
 
 export const signAndSubmitTx = async (tx: TxComplete): Promise<TxHash> => {
     const signedTx = await tx.sign().complete();
@@ -39,14 +47,21 @@ export const findUTxO = async (lucid: Lucid, ref: string) => {
 // create personalised metadata
 export function genPersolanizedMetadata(name: string) {
     return {
-        name: GENERIC_METADATA.name + name,
-        image: GENERIC_METADATA.image,
-        description: GENERIC_METADATA.description,
+        name: genericMetadata.name + name,
+        image: genericMetadata.image,
+        description: genericMetadata.description,
     };
 }
 
-export function genMerkleRoot() {
-    const dataUint = MERKLE_DATA.map((x) => fromHex(x));
+export function genStartMerkleRoot() {
+    const dataUint = startMerkleTreeData.map((x) => fromHex(x));
+    const merkleTree = new MerkleTree(dataUint);
+    const merkleRoot: Hash32 = { hash: toHex(merkleTree.rootHash()) };
+    return merkleRoot;
+}
+
+export function genMintMerkleRoot() {
+    const dataUint = mintMerkleTreeData.map((x) => fromHex(x));
     const merkleTree = new MerkleTree(dataUint);
     const merkleRoot: Hash32 = { hash: toHex(merkleTree.rootHash()) };
     return merkleRoot;
@@ -57,30 +72,36 @@ export async function participantNumberToMerkleData(
     pubkeyhash: string
 ): Promise<string> {
     const plutusMetaData = Data.fromJson(genPersolanizedMetadata(name));
-    const datum = Data.to(new Constr(0, [plutusMetaData]));
-    const datumHash = await (
-        await fetch("api/hash", {
-            method: "POST",
-            headers: {
-                "Content-type": "application/json",
-            },
-            body: JSON.stringify({ datum: datum }),
-        })
-    ).json();
-
-    console.log("pkh+datumHash: " + pubkeyhash + datumHash.hash);
-
-    return pubkeyhash + datumHash.hash;
+    const datumHash = toHex(
+        C.hash_blake2b256(fromHex(Data.to(new Constr(0, [plutusMetaData]))))
+    );
+    console.log("pkh+datumHash: " + pubkeyhash + datumHash);
+    return pubkeyhash + datumHash;
 }
 
-export async function genMerkleProof(name: string, pkh: string) {
-    const dataUint = MERKLE_DATA.map((x) => fromHex(x));
+export async function genStartMerkleProof(participantId: number) {
+    const dataUint = startMerkleTreeData.map((x) => fromHex(x));
+    const merkleTree = new MerkleTree(dataUint);
+
+    const merkleProof: MerkleProof = merkleTree
+        .getProof(dataUint[participantId])
+        .map((p) =>
+            p.left
+                ? { Left: [{ hash: toHex(p.left) }] }
+                : { Right: [{ hash: toHex(p.right!) }] }
+        );
+
+    return merkleProof;
+}
+
+export async function genMintMerkleProof(name: string, pkh: string) {
+    const dataUint = mintMerkleTreeData.map((x) => fromHex(x));
     const merkleTree = new MerkleTree(dataUint);
 
     // nft datum
     const metadataDatum = await participantNumberToMerkleData(name, pkh);
     // retrieve the index of the user in the list of merkle tree data.
-    const n = MERKLE_DATA.indexOf(metadataDatum);
+    const n = mintMerkleTreeData.indexOf(metadataDatum);
     console.log("index of th user in the list: " + n);
 
     const merkleProof: MerkleProof = merkleTree
@@ -92,4 +113,41 @@ export async function genMerkleProof(name: string, pkh: string) {
         );
 
     return merkleProof;
+}
+
+export function setBit(hexStr: string, bitIndex: number): string {
+    const newStr = (
+        (BigInt(1) << BigInt(bitIndex)) |
+        BigInt("0x" + hexStr)
+    ).toString(16);
+    return newStr.padStart(hexStr.length, "0");
+}
+
+function countSetBits(hexStr:string) {
+    let n = BigInt("0x" + hexStr);
+    let count = 0n;
+    while (n) {
+        n &= (n - 1n);
+        count++;
+    }
+    return count;
+}
+
+export async function numberOfExamsStarted(
+    lucid: Lucid
+): Promise<number | undefined> {
+    const stateValidator: SpendingValidator =
+        setupData.stateValidator as SpendingValidator;
+    const stateAddress: Address =
+        lucid.utils.validatorToAddress(stateValidator);
+    const stateTkn: Unit = setupData.stateToken;
+    const utxoAtScript: UTxO[] = await lucid.utxosAt(stateAddress);
+    const ourUTxO: UTxO[] = utxoAtScript.filter(
+        (utxo) => utxo.assets[stateTkn] == 1n
+    );
+
+    if (ourUTxO && ourUTxO.length > 0 && ourUTxO[0].datumHash) {
+        const stateDatum = await lucid.provider.getDatum(ourUTxO[0].datumHash);
+        return Number(countSetBits(Data.from(stateDatum)))
+    }
 }
