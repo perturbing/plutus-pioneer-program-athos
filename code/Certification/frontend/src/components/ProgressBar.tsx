603073c479d39f66c4a0fbc3c6165cd8be6a1cf0
Robertino Martinez <48034748+rober-m@users.noreply.github.com>
2023-06-26 09:22:17 +0000

    fixed progessbar

--- code/Certification/frontend/src/components/ProgressBar.tsx
+++ code/Certification/frontend/src/components/ProgressBar.tsx
@@ -6,7 +6,7 @@ const ProgressBar = ({ progress }: IProgressBar) => {
     return (
         <div className="w-full overflow-hidden h-2 mb-4 text-xs flex rounded bg-gray-200">
             <div
-                style={{ width: `${progress}%` }}
+                style={{ width: `${progress * 100}%` }}
                 className="shadow-none flex flex-col text-center whitespace-nowrap text-white justify-center bg-green-500"
             ></div>
         </div>
